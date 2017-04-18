86180606fad712bdf91445743d2a3f491d4609db
negikdeep <negikdeep@negiks>
2017-04-18 22:03:53 +0530

    Adding Test1 By Tom

---
+++ Test Suites/TestSuite 1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2017-04-18T23:44:03</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2d25b41c-298b-485c-beb2-67f06e6be11f</testSuiteGuid>
+</TestSuiteEntity>
